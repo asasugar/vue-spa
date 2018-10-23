3d98109e432092e1f730cfd6b8882d7d7d966398
xxj95719 <87416959@qq.com>
2018-10-23 09:01:41 +0800

    axios拦截器

--- src/utils/js/request.js
+++ src/utils/js/request.js
@@ -2,7 +2,29 @@ import axios from "axios";
 
 const BASE_PATH = "https://api.com/";
 axios.defaults.withCredentials = true;
+// Add a request interceptor
+axios.interceptors.request.use(
+  function(config) {
+    // Do something before request is sent
+    return config;
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
 
+// Add a response interceptor
+axios.interceptors.response.use(
+  function(response) {
+    // Do something with response data
+    return response;
+  },
+  function(error) {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 const request = (
   { url = "/", data = {}, type = "get" } = {},
   otherConfig = {}
